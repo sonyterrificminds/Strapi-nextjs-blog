175fa4e6e0d6ad1caae7ea77d8a5bd705788a73a
sonyalphy <sony@terrificminds.com>
2021-11-12 19:52:24 +0530

    strapi -starter blog demo

---
+++ my-project/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ee6ddec9afb7060d5ce41003a595fe52'),
+    },
+  },
+});
---
+++ my-project/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fac683a0-9c0b-4daf-bd67-415f87a1fb7f'
+};
